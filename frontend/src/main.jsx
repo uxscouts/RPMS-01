8d0c3d7be6770715efad598c82cdc8d11cba2fc6
UX SCOUTS <admin@uxscouts.com>
2026-06-25 15:42:07 +0000

    front page sort of

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,16 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <h1>TimeCzar</h1>
+    <nav class="navbar">
+      <ul class="menu-grid">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
     <App />
   </StrictMode>,
 )
